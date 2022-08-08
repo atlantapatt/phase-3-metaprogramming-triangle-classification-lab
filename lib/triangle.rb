7732b82981596fe53bb871a60936cf81f8d60104
AtlantaPatterson <atlantarpatterson@gmail.com>
2022-08-08 14:14:07 -0700

    commit tree 7fd01961edad9ce4905ee37cd2baff0112745f32

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
 class Triangle
-  # write code here
+  attr_accessor :kind
+  def initialize(length1, length2, length3)
+    @lengths = []
+      @length1 = length1
+      @length2 = length2
+      @length3 = length3
+
+      @lengths << length1
+      @lengths << length2
+      @lengths << length3
+  end
+  
+  def kind
+     if @length1 > 0 && @length2 > 0 && @length3 > 0 && @length1 + @length2 > @length3 && @length2 + @length3 > @length1 && @length1 + @length3 > @length2
+      if @lengths.uniq.length == 1
+        @kind = :equilateral
+      elsif @lengths.uniq.length == 2
+        @kind = :isosceles
+      elsif @lengths.uniq.length == 3
+        @kind = :scalene
+      end
+    else 
+      raise TriangleError
+    end
+  end
+    class TriangleError < StandardError
+      def message
+        "this is not a triangle!"
+      end
+  end
 end
